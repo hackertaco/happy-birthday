f436f74c0210a17c81ea041962e8a41f1fcabeda
hackertaco <imsa1224@naver.com>
2020-11-05 17:54:54 +0900

    feat: ⚡️ mk login page

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,5 +1,32 @@
 <template>
-Login
+<div class="tracking-tight w-screen h-screen bg-red-700 m-auto fixed" style="font-family:SpaceMono">
+    <img class="w-full h-4" src="../assets/cautionLine.jpg" />
+    <div class="text-white m-auto justify-center h-full flex items-center flex-col w-1/2">
+        <svg class="w-1/12" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+        </svg>
+        <h1 class="text-3xl my-4">사이트 접속이 차단되었습니다.</h1>
+        <span class="text-yellow-500">오직 해홍이만 들어올 수 있음을 알려드립니다.</span>
+        <span>다만 해홍이도 로그인해야합니다.</span>
+        <form class="shadow-2xl shadow-inner text-gray-600 bg-white my-6 w-2/5 flex items-center flex-col  rounded-3xl" @submit.prevent="submit">
+            <div class="w-4/5 my-2">
+                <div class="my-3 w-full flex flex-col">
+                    <label>ID</label>
+                    <input class=" border-b border-gray-300 focus:border-red-600 text-black p-1 w-full" type="text" placeholder="홍해인의 정승아 카톡명" />
+                </div>
+                <div class="my-4 w-full flex flex-col">
+                    <label for="">Password</label>
+                    <input class="border-b border-gray-300 focus:border-red-600 p-1 w-full text-black" type="password" placeholder="홍해인의 생년월일 8 숫자" />
+                </div>
+                <div class="my-3 mx-3 text-center ">
+                    <button type="submit" class="p-1 cursor-pointer w-1/4 hover:bg-yellow-500 hover:text-white rounded-lg shadow-md border text-sm">Login</button>
+                </div>
+            </div>
+        </form>
+    </div>
+    <footer class="bottom-0 right-0 absolute text-white text-xs">@copyright tacos_chocos</footer>
+
+</div>
 </template>
 
 <script>
