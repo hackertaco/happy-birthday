51b7e22821ab15e31925536572e5046f45e74f5e
hackertaco <imsa1224@naver.com>
2020-11-11 03:31:36 +0900

    feat: ⚡️route last

--- src/router.js
+++ src/router.js
@@ -2,10 +2,12 @@ import {createRouter, createWebHistory} from 'vue-router';
 import Login from './pages/Login';
 import Letter from './pages/Letter'
 import Present from './pages/Present'
+import Last from './pages/Last'
 const routes = [
     {path: '/', component:Login},
     {path:'/letter',component:Letter},
-    {path: '/present', component:Present}
+    {path: '/present', component:Present},
+    {path: '/last', component:Last}
 ]
 
 const router = createRouter({
